d508fdab760f03609a7153ef04d2b93cbcbfbb8d
Ammon Jeffery <savingphace@gmail.com>
2020-06-24 21:49:37 -0600

    created updateDailyTasks fuction

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ let entireWorkDay = {
     "4 PM" : "",
     "5 PM" : "",
 };
-
+// Since I labeled several ids on each row numerically, setting this variable will let me more easily reference said ids.
 let counter = 1;
+//Initial function on page load. Sets local storage if there is none currently. Otherwise, it parses what is already there.
+$(document).ready(function() {
+    if (!localStorage.getItem('entireWorkDay')){
+        updateDailyTasks(entireWorkDay);
+    } else {
+        updateDailyTasks(JSON.parse(localStorage.getItem('entireWorkday')))
+    }
+});
 
+function updateDailyTasks (workDayObject){
+ $(".calender-row").each(function(index){
+    let texting = $(this).children("div");
+    $(this).children("textarea").text(dayObject[texting.text()])
+ })   
+}
